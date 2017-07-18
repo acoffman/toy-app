ee61815000bf3d748f7ae0521cbc3972a31113c8
Adam Coffman <acc@fastmail.com>
2017-07-18 13:05:36 -0500

    count user visits in the controller

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,10 +1,17 @@
 class GreeterController < ApplicationController
   def hello
-    @name = params[:name].upcase
-    @params = params
+    name = params[:name].upcase
+    @user = UserVisit.find_by(name: name)
+    if @user.present?
+      @user.visit_count +=1
+      @user.save
+    else
+      @user = UserVisit.create(name: @name, visit_count: 1)
+    end
   end
 
-  def generic
-    @name = 'Stranger'
+  def index
+    @top_users = UserVisit.order('visit_count desc')
+      .limit(10)
   end
 end
